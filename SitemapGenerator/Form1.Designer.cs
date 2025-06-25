57e5b626e24352e0afb3950fa6b7414338318484
Hossein <assefi.hossein@gmail.com>
2025-06-25 09:10:14 +0330

    initial commit

---
+++ SitemapGenerator/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace SitemapGenerator
+{
+    partial class frmSitemapGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUri = new System.Windows.Forms.TextBox();
+            this.btnStartCrawling = new System.Windows.Forms.Button();
+            this.txtExclude = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Uri To Crawl";
+            // 
+            // txtUri
+            // 
+            this.txtUri.Location = new System.Drawing.Point(83, 33);
+            this.txtUri.Name = "txtUri";
+            this.txtUri.Size = new System.Drawing.Size(278, 20);
+            this.txtUri.TabIndex = 1;
+            this.txtUri.Text = "https://your-domain.com";
+            // 
+            // btnStartCrawling
+            // 
+            this.btnStartCrawling.Location = new System.Drawing.Point(15, 223);
+            this.btnStartCrawling.Name = "btnStartCrawling";
+            this.btnStartCrawling.Size = new System.Drawing.Size(111, 23);
+            this.btnStartCrawling.TabIndex = 2;
+            this.btnStartCrawling.Text = "Start Crawling";
+            this.btnStartCrawling.UseVisualStyleBackColor = true;
+            this.btnStartCrawling.Click += new System.EventHandler(this.btnStartCrawling_Click);
+            // 
+            // txtExclude
+            // 
+            this.txtExclude.Location = new System.Drawing.Point(83, 71);
+            this.txtExclude.Name = "txtExclude";
+            this.txtExclude.Size = new System.Drawing.Size(278, 122);
+            this.txtExclude.TabIndex = 3;
+            this.txtExclude.Text = "https://domain.com/mag/,page";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 83);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Exclude links that contain\r\n\r\nSeprate by Camma(,)";
+            // 
+            // frmSitemapGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(379, 258);
+            this.Controls.Add(this.txtExclude);
+            this.Controls.Add(this.btnStartCrawling);
+            this.Controls.Add(this.txtUri);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSitemapGenerator";
+            this.Text = "Sitemap Generator - Web Crawler";
+            this.Load += new System.EventHandler(this.frmSitemapGenerator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUri;
+        private System.Windows.Forms.Button btnStartCrawling;
+        private System.Windows.Forms.RichTextBox txtExclude;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SitemapGenerator/Form1.cs
@@ -0,0 +1,125 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SitemapGenerator
+{
+    public partial class frmSitemapGenerator : Form
+    {
+        public frmSitemapGenerator()
+        {
+            InitializeComponent();
+        }
+        List<KeyValuePair<string, int>> linkList = new List<KeyValuePair<string, int>>();
+        List<string> queueToCrawl = new List<string>();
+
+        private void frmSitemapGenerator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartCrawling_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtUri.Text.Trim()))
+            {
+                HtmlWeb web = new HtmlWeb();
+
+                HtmlAgilityPack.HtmlDocument htmlDoc = web.Load(txtUri.Text);
+                Uri uri = new Uri(txtUri.Text.Trim());
+
+
+                var node = htmlDoc.DocumentNode.SelectNodes("//a");
+                var x = node.ToList().Where(s => !linkList.Any(s2 => s2.Key.ToLower().Equals(s.Attributes["href"].Value.ToLower()))).ToList();
+                List<KeyValuePair<string, int>> y = (from q in x
+                                                     where
+                                                     q.Attributes["href"] != null && !q.Attributes["href"].Value.Contains("#") && !q.Attributes["href"].Value.Contains("twitter") && !q.Attributes["href"].Value.Contains("facebook") &&
+                                !q.Attributes["href"].Value.Contains("whatsapp") && !q.Attributes["href"].Value.Contains("telegram") &&
+                                !q.Attributes["href"].Value.Contains("mailto") &&
+                                (q.Attributes["href"].Value.Contains(uri.Host) || q.Attributes["href"].Value.Contains("products?page"))
+                                                     select new KeyValuePair<string, int>(q.Attributes["href"].Value, new Uri(q.Attributes["href"].Value.Contains(uri.Host) ? q.Attributes["href"].Value : (uri.AbsoluteUri + q.Attributes["href"].Value)).Segments.Count())).Distinct().ToList();
+
+                linkList.AddRange(y);
+                queueToCrawl.AddRange(y.Select(z => z.Key));
+                while (queueToCrawl.Count > 0)
+                {
+                    htmlDoc = web.Load(queueToCrawl[0].Contains(uri.Host) ? queueToCrawl[0] : (uri.AbsoluteUri + queueToCrawl[0]));
+                    node = htmlDoc.DocumentNode.SelectNodes("//a");
+                    if (node != null)
+                    {
+                        x = node.ToList().Where(s => !linkList.Any(s2 => s2.Key.ToLower().Equals(s.Attributes["href"]?.Value.ToLower()))).ToList();
+                        y = (from q in x
+
+                             where 
+                             q.Attributes["href"] != null && 
+                             !q.Attributes["href"].Value.Contains("#") &&
+                                !q.Attributes["href"].Value.Contains("mailto") && !q.Attributes["href"].Value.Contains("twitter") && !q.Attributes["href"].Value.Contains("facebook") &&
+                                !q.Attributes["href"].Value.Contains("whatsapp") && !q.Attributes["href"].Value.Contains("telegram") &&
+                                (q.Attributes["href"].Value.Contains(uri.Host) || q.Attributes["href"].Value.Contains("products?page"))
+                             select new KeyValuePair<string, int>(q.Attributes["href"].Value.Trim().Replace(" ", "").ToLower(), new Uri(q.Attributes["href"].Value.Trim().Replace(" ", "").Contains(uri.Host) ? q.Attributes["href"].Value.Trim().Replace(" ", "") : (uri.AbsoluteUri + q.Attributes["href"].Value.Trim().Replace(" ", ""))).Segments.Count())).Distinct().ToList();
+                        linkList.AddRange(y);
+                        queueToCrawl.AddRange(y.Select(z => z.Key));
+                    }
+                    queueToCrawl.RemoveAt(0);
+                }
+                StringBuilder sb = new StringBuilder();
+                string priority;
+                sb.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>").AppendLine("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+                for (int i = 0; i < linkList.Count; i++)
+                {
+                    if (!string.IsNullOrEmpty( txtExclude.Text.Trim()))
+                    {
+                        string[] excludeItems = txtExclude.Text.Trim().Split(',');
+                        for (int z = 0; z < excludeItems.Length; z++)
+                        {
+                            if (linkList[i].Key.Contains(excludeItems[z].ToLower()))
+                            {
+                                goto End;
+                            }
+                        }
+                    }
+                    if (linkList[i].Value == 1)
+                        priority = "0.86";
+                    else if (linkList[i].Value == 2)
+                        priority = "0.79";
+                    else if (linkList[i].Value == 3)
+                        priority = "0.74";
+                    else if (linkList[i].Value == 4)
+                        priority = "0.69";
+                    else if (linkList[i].Value == 5)
+                        priority = "0.64";
+                    else if (linkList[i].Value == 6)
+                        priority = "0.59";
+                    else
+                        priority = "0.5";
+
+                    sb.AppendLine("<url>").Append("<loc>").Append(linkList[i].Key.Replace("&", "&amp;").Trim().StartsWith("pro") ? (uri.AbsoluteUri + linkList[i].Key.Replace("&", "&amp;").Trim()) : linkList[i].Key.Replace("&", "&amp;").Trim()).Append(linkList[i].Key.Replace("&", "&amp;").Trim().EndsWith("/") ? "" : "/").AppendLine("</loc>")
+                        .Append("<lastmod>").Append(DateTime.Now.Date.ToString("yyyy-MM-dd")).AppendLine("</lastmod>")
+                        .Append("<priority>").Append(priority).AppendLine("</priority>")
+                        .AppendLine("</url>");
+
+                End:;
+
+                }
+                sb.Append("</urlset>");
+                StreamWriter writer = new StreamWriter(Environment.CurrentDirectory + @"\sitemap.xml");
+                writer.Write(sb.ToString());
+                writer.Close();
+                MessageBox.Show("Sitemap Generated Successfully", "Site Map Genetator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("Enter The Uri", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUri.Focus();
+            }
+        }
+    }
+}
